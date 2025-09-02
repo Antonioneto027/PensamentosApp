07e776e18b457a83d68384353bba423bca26cc8e
Antonioneto027 <web.everybody822@passinbox.com>
2025-09-02 06:19:16 -0300

    PHP Mailer set up

--- App/Helpers/Mail.php
+++ App/Helpers/Mail.php
@@ -1,13 +1,13 @@
 <?php
 //Import PHPMailer classes into the global namespace
 //These must be at the top of your script, not inside a function
-use Helpers\MailConfig;
+use HelpersTest\MailConfig;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
  
 //Load Composer's autoloader (created by composer, not included with PHPMailer)
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
  
 //Create an instance; passing `true` enables exceptions
  
@@ -23,7 +23,7 @@ class Mail extends PHPMailer {
 
 
         $this->mail = new PHPMailer(true);
-        $this->config = new MailConfig() ;
+        $this->config = new MailConfig();
 
     }
 
@@ -32,26 +32,37 @@ class Mail extends PHPMailer {
 
         try {
             //Server settings
-            $this->config;
+                  
+            $this->mail->isSMTP();
+            $this->mail->Host = 'live.smtp.mailtrap.io';
+            $this->mail->SMTPAuth = true;
+            $this->mail->Port = 587;
+            $this->mail->Username = 'smtp@mailtrap.io';
+            $this->mail->Password = 'f3e9f0655c410da4905e28bb6befeae4';
+            $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+            $this->mail->Port       = 587;         
+            $this->mail->CharSet = 'UTF-8';
+            
             //Recipients
-            $this->mail->setFrom('from@example.com', 'Mailer');
-            $this->mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
-            $this->mail->addAddress('ellen@example.com');               //Name is optional
-            $this->mail->addReplyTo('info@example.com', 'Information');
-            $this->mail->addCC('cc@example.com');   
-            $this->mail->addBCC('bcc@example.com');
+            $this->mail->setFrom('no-reply@pensamentos.app.br', 'Pensamentos App');
+            $this->mail->addAddress('web.everybody822@passinbox.com', 'Antonio');     //Add a recipient
+            //$this->mail->addAddress('ellen@example.com');               //Name is optional
+            //$this->mail->addReplyTo('info@example.com', 'Information');
+            //$this->mail->addCC('cc@example.com');   
+            //$this->mail->addBCC('bcc@example.com');
 
             //Attachments
-            $this->mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-            $this->mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+            //$this->mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+            //$this->mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
             //Content
             $this->mail->isHTML(true);                                  //Set email format to HTML
-            $this->mail->Subject = 'Here is the subject';
-            $this->mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-            $this->mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+            $this->mail->Subject = 'E-mail teste';
+            $this->mail->Body    = 'Olá!';
+            $this->mail->AltBody = 'Olá';
 
             $this->mail->send();
+
             echo 'Message has been sent';
 
         } catch (Exception $e) {
@@ -61,4 +72,14 @@ class Mail extends PHPMailer {
     }
     
 }
+
+    $mail = new Mail();
+    $mail->sendMail(); 
+
+?>
+
+
+
+
+
  
--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -7,4 +7,5 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'Composer\\InstalledVersions' => $vendorDir . '/composer/InstalledVersions.php',
+    'HelpersTest\\MailConfig' => $baseDir . '/Labs/MailConfig.php',
 );
